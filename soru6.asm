b90ebd7deacf89410db35403910cad1c92887085
21haniya <122908383+21haniya@users.noreply.github.com>
2024-03-02 18:23:27 +0300

    Update soru6.asm

--- soru6.asm
+++ soru6.asm
@@ -1,7 +1,7 @@
 
 
 ;Kullanicidan alacagi 5 karakteri, (10,5) 
-;konumundan baslayarak her karakterden 3 tane, eðer 
+;konumundan baslayarak her karakterden 3 tane, eÄŸer 
 ;karakterler ayni ise yan yana, 
 ;farkli ise 5. sutunu bozmadan alt alta yazdiran 
 ;8086 Assembly kodunu yaziniz
@@ -10,6 +10,8 @@
 
 org 100h 
 
+;bu soru icin donguye girmeden 
+;once karsilastiracak bir deger aliyoruz
 
 mov ah,02h ; imlec pozisyonunu ayarlama
 mov dh, 10h ;satir
@@ -20,7 +22,7 @@ int 10h
 
 MOV AH,00h
 int 16h ; klavyeden veri al 
-MOV temp, AL 
+MOV temp, AL ;***
  
 
 MOV AH,09h ; imlec karakter yazma
@@ -34,24 +36,24 @@ MOV AH,03h ; imlec pozisyon okuma
 int 10h ; DH satir, DL de sutun degeri var
 
 
-MOV CX,4  
+MOV CX,4  ; biri yukarida alindi geriye 4 kaldi
 
 
 don:   
 
-JCXZ bitir
+JCXZ bitir ;bitince sonlan yani
 PUSH CX   
 
 
 MOV AH,00h
 int 16h ; klavyeden veri al
 
-
+;simdi karsilastir
 CMP temp, AL
-JE ayni 
+JE ayni ; ayni degilse devam et diyoruz
 
 
-MOV temp, AL   
+MOV temp, AL   ; al de tutulan cevabi temp aliyor
 
 
 MOV AH,03h ; imlec pozisyon okuma
@@ -59,8 +61,8 @@ int 10h ; DH satir, DL de sutun degeri var
 
 
 MOV AH, 02h ;imlec pozisyon ayarlama
-MOV DL, 5h ;farkliysa sutun ayni kalacak satir artacak asagi inecek
-INC DH
+MOV DL, 5h ;farkliysa sutun ayni kalacak satir artacak asagi inecek, ;sutun ayni tutuluyor
+INC DH ;satir arttiriliyor
 int 10h
 
 
@@ -71,7 +73,7 @@ MOV BL, 00110000b
 int 10h 
 
 
-jmp devam 
+jmp devam ;ayniya girmeden devam
 
 ayni: 
 
